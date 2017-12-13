fe71479063cbe161a738c987e0bac831e5e4d07a
Adrien BARRAL <aba@robopec.com>
2017-12-13 07:21:58 +0100

    DOCKER ok.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/photo.hpp>
+#include <opencv2/photo/photo.hpp>
 #include <iostream>
 #include <sstream>
 
